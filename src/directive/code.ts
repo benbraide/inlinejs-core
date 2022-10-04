791f66e2d98117f112bc603b7602b60d0394cd18
benbraide <benbraide@yahoo.com>
2022-10-04 09:00:04 +0100

    'x-code' directives requires a template element

--- src/directive/code.ts
+++ src/directive/code.ts
@@ -1,10 +1,18 @@
-import { AddDirectiveHandler, CreateDirectiveHandlerCallback, EvaluateLater, GetGlobal } from "@benbraide/inlinejs";
+import { AddDirectiveHandler, CreateDirectiveHandlerCallback, EvaluateLater, GetGlobal, JournalError } from "@benbraide/inlinejs";
 import { ICodeConcept } from "../types";
 
-export const CodeDirectiveHandler = CreateDirectiveHandlerCallback('code', ({ componentId, contextElement, expression, argKey }) => {
-    let content = (contextElement.textContent || '').trim();
-    contextElement.textContent && GetGlobal().GetConcept<ICodeConcept>('code')?.AddBlock(expression, content);
-    (argKey === 'execute') && EvaluateLater({ componentId, contextElement, expression: content, disableFunctionCall: true })();
+export const CodeDirectiveHandler = CreateDirectiveHandlerCallback('code', ({ componentId, contextElement, expression, argKey, originalView }) => {
+    if (!(contextElement instanceof HTMLTemplateElement)){
+        JournalError('Target is not a template element.', `'${originalView}'.Init`, contextElement);
+        return;
+    }
+
+    expression = expression.trim();
+    
+    let content = (contextElement.content.textContent || '').trim();
+    expression && content && GetGlobal().GetConcept<ICodeConcept>('code')?.AddBlock(expression, content);
+    
+    (argKey !== 'tmpl' && argKey !== 'template') && EvaluateLater({ componentId, contextElement, expression: content, disableFunctionCall: true })();
 });
 
 export function CodeDirectiveHandlerCompact(){
