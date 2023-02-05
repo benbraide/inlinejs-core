108633f2120c156a7c39b34348bd2ea9db7eaa8f
benbraide <benbraide@yahoo.com>
2023-02-05 19:19:07 +0100

    Feature update

--- lib/esm/magic/data/stream.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const StreamMagicHandler: import("@benbraide/inlinejs").IMagicHandlerCallbackDetails;
-export declare function StreamMagicHandlerCompact(): void;
--- lib/esm/magic/data/wait.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const WaitMagicHandler: import("@benbraide/inlinejs").IMagicHandlerCallbackDetails;
-export declare function WaitMagicHandlerCompact(): void;
